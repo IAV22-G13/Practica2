3b55a159a32f3f90324ea05f1dea5d04c84106e3
albemu07 <albemu07@ucm.es>
2022-03-04 19:05:23 +0100

    Movimiento y prefab del player a través de imput

---
+++ Navegacion/Assets/Scripts/Agentes/Direccion.cs
@@ -0,0 +1,30 @@
+/*    
+   Copyright (C) 2020 Federico Peinado
+   http://www.federicopeinado.com
+   Este fichero forma parte del material de la asignatura Inteligencia Artificial para Videojuegos.
+   Esta asignatura se imparte en la Facultad de Informática de la Universidad Complutense de Madrid (España).
+   Autor: Federico Peinado 
+   Contacto: email@federicopeinado.com
+*/
+namespace UCM.IAV.Movimiento
+{
+
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Clase auxiliar para representar la dirección (instrucciones) con la que corregir el movimiento dinámicamente, mediante aceleraciones.
+    /// </summary>
+    public class Direccion
+    {
+        public float angular;
+        public Vector3 lineal;
+        public Direccion()
+        {
+            angular = 0.0f;
+            lineal = new Vector3();
+        }
+    }
+}
