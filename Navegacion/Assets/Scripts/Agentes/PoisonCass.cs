64810a008eac3960aa9e4f61333faa732167d130
oscar <osgarc05@ucm.es>
2022-03-13 23:11:36 +0100

    Area de efecto del minotauro

--- Navegacion/Assets/Scripts/Agentes/PoisonCass.cs
+++ Navegacion/Assets/Scripts/Agentes/PoisonCass.cs
@@ -12,24 +12,30 @@ namespace UCM.IAV.Navegacion
         Vertex[] vList = new Vertex[0];
         Vertex lastCass = null;
         Vector2 id;
+        public Material actualCass, lowerCass, normal;
         public void Update()
         {
             if (lastCass != null)
             {
                 id = gP.IdToGrid(lastCass.id);
                 gP.mapVertices[(int)id.x, (int)id.y] = true;
+                lastCass.GetComponent<MeshRenderer>().material = normal;
             }
             for (int i = 0; i < vList.Length; i++)
             {
                 gP.costs[vList[i].id] = clearCost;
+                vList[i].GetComponent<MeshRenderer>().material = normal;
             }
             vList = gP.poison(this.gameObject);
-            id = gP.IdToGrid(lastCass.id);
             lastCass = gP.GetNearestVertex(this.transform.position);
+            id = gP.IdToGrid(lastCass.id);
+            lastCass.GetComponent<MeshRenderer>().material = actualCass;
             gP.mapVertices[(int)id.x, (int)id.y] = false;
+
             for (int i = 0; i < vList.Length; i++)
             {
                 gP.costs[vList[i].id] = poisonedCost;
+                vList[i].GetComponent<MeshRenderer>().material = lowerCass;
             }
         }
     }
